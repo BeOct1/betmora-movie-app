98ebf1dbb8e4d92ddb28284eac4786db5e94a639
BeOct1 <bitrusmail@gmail.com>
2025-06-20 16:30:00 +0100

    feat: add main application structure with routing

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+export default function App() {
+    return (
+        <Routes>
+            <Route path="/" element={<div>Hello from Betmora</div>} />
+        </Routes>
+    );
+}
+
